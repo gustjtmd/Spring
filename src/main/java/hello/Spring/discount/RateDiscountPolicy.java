0293cfbf4b0edd9b5976fbd0892b66b882dbf89d
seoseunghyeon <gustjtmd@gmail.com>
2022-02-17 22:30:07 +0900

    Spring

--- src/main/java/hello/Spring/discount/RateDiscountPolicy.java
+++ src/main/java/hello/Spring/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.Spring.discount;
 
 import hello.Spring.member.Grade;
 import hello.Spring.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
---
+++ src/test/java/hello/Spring/scan/AutoAppConfigTest.java
@@ -0,0 +1,18 @@
+package hello.Spring.scan;
+
+import hello.Spring.AutoAppConfig;
+import hello.Spring.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
